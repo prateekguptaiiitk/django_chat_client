1ac7d808cacc2a6caaebf46b7d07555166e4cdc6
Prateek Gupta <prateekjpg@gmail.com>
2025-12-19 17:11:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import Routes from "./Routes";
+
+function App() {
+  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.withCredentials = true;
+  axios.xsrfCookieName = 'csrftoken';
+  axios.xsrfHeaderName = 'X-CSRFToken';
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  )
+}
+
+export default App
