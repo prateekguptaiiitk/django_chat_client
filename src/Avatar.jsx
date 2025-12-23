f7e007612358c96ab9550243eb23d396ae15772b
Prateek Gupta <prateekjpg@gmail.com>
2025-12-23 16:50:08 +0530

    Add working code for messaging feature

--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -3,11 +3,11 @@ export default function Avatar({userId,username,online}) {
                   'bg-green-200', 'bg-purple-200',
                   'bg-blue-200', 'bg-yellow-200',
                   'bg-orange-200', 'bg-pink-200', 'bg-fuchsia-200', 'bg-rose-200'];
-  const userIdBase10 = parseInt(userId.substring(10), 16);
-  const colorIndex = userIdBase10 % colors.length;
+  const userIdBase10 = parseInt(userId, 16);
+  const colorIndex = userId % colors.length;
   const color = colors[colorIndex];
   return (
-    <div className={"w-8 h-8 relative rounded-full flex items-center "+color}>
+    <div className={"w-8 h-8 relative rounded-full flex items-center " + color}>
       <div className="text-center w-full opacity-70">{username[0]}</div>
       {online && (
         <div className="absolute w-3 h-3 bg-green-400 bottom-0 right-0 rounded-full border border-white"></div>
